2e4724f05585a693873527c453e17a68b5ef50b9
tkk <t.kleylein@web.de>
2023-12-02 10:20:39 +0100

    add general day-structured directory and  day two

---
+++ 2023/day_x/day_x.py
@@ -0,0 +1,25 @@
+from utils import lines_of_file
+
+def part_one(file):
+    res = 0
+    for line in lines_of_file(file):
+        ...
+    return res
+
+
+def part_two(file):
+    res = 0
+    for line in lines_of_file(file):
+        ...
+    return res
+
+
+if __name__ == '__main__':
+    res = part_one('day_x_input_ex.txt')
+    print(f"Beispiel 1: {res}")
+    res = part_one('day_x_input.txt')
+    print(f"Ergebnis 1: {res}")
+    res = part_two('day_2_input_ex2.txt')
+    print(f"Beispiel 2: {res}")
+    res = part_two('day_x_input.txt')
+    print(f"Ergebnis 2: {res}")
