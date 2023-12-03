e938b3f23b0bd79c78d30654c13a287e34bb9282
tkk <t.kleylein@web.de>
2023-12-03 13:21:27 +0100

    solution day 3

--- 2023/day_3/day_3.py
+++ 2023/day_3/day_3.py
@@ -1,25 +1,63 @@
 from utils import lines_of_file
+import re
 
-def part_one(file):
-    res = 0
-    for line in lines_of_file(file):
-        ...
-    return res
+def check_special_character(gear, line_part, idx, check_range):
+    special_character = re.findall('[^0-9.]', line_part)
+    if len(special_character) > 0:
+        gear['line'] = idx - 1
+        gear['position'] = line_part.index(special_character[0]) + check_range.start
+        if special_character[0] == '*':
+            gear['is_gear'] = True
+    return gear
 
+def run(file):
+    res_1 = 0
+    res_2 = 0
+    lines = [l for l in lines_of_file(file)]
+    gears = {}
+    for idx, line in enumerate(lines):
+        numbers = re.findall('[0-9]+', line)
+        for number in numbers:
+            gear = {'is_gear': False, 'line': -1, 'position': -1}
+            start_index = line.index(number)
+            stop_index = start_index + len(number)
+            check_range = range(start_index - 1, stop_index + 1)
+            if check_range.start < 0:
+                check_range = range(0 , check_range.stop)
+            if check_range.stop > len(line) - 1:
+                check_range = range(check_range.start, len(line))
 
-def part_two(file):
-    res = 0
-    for line in lines_of_file(file):
-        ...
-    return res
+            if idx > 0:
+                check_before = lines[idx - 1][check_range.start:check_range.stop]
+                gear = check_special_character(gear, check_before, idx - 1, check_range)
+
+            check_current = line[check_range.start:check_range.stop]
+            gear = check_special_character(gear, check_current, idx, check_range)
+
+            if idx + 1 < len(lines):
+                check_after = lines[idx + 1][check_range.start:check_range.stop]
+                gear = check_special_character(gear, check_after, idx + 1, check_range)
+
+            if gear['line'] >= 0 and gear['position'] >= 0:
+                res_1 += int(number)
+                if gear['is_gear'] is True:
+                    gear_name = f"{gear['line']}-{gear['position']}"
+                    if gear_name in gears:
+                        res_2 += gears[gear_name] * int(number)
+                    else:
+                        gears[gear_name] = int(number)
+
+            # handle double numbers
+            line = line.replace(number, '0' * len(number), 1)
+
+    return res_1, res_2
 
 
 if __name__ == '__main__':
-    res = part_one('day_3_input_ex.txt')
-    print(f"Beispiel 1: {res}")
-    res = part_one('day_3_input.txt')
-    print(f"Ergebnis 1: {res}")
-    res = part_two('day_3_input_ex.txt')
-    print(f"Beispiel 2: {res}")
-    res = part_two('day_3_input.txt')
-    print(f"Ergebnis 2: {res}")
+    res_1, res_2 = run('day_3_input_ex.txt')
+    print(f"Beispiel 1: {res_1}")
+    print(f"Beispiel 2: {res_2}")
+    res_1, res_2 = run('day_3_input.txt')
+    print(f"Ergebnis 1: {res_1}")
+    print(f"Ergebnis 2: {res_2}")
+
